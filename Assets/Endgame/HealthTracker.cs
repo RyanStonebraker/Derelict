3b47b3879290eaaaaf6df17110d20751861db2b4
Ryan Stonebraker <ryanastonebraker@gmail.com>
2018-02-23 03:48:22 -0900

    Implemented explosions in scene and started working on end game state.
    Created health tracker class that monitors for end game scenarios.

---
+++ Assets/Endgame/HealthTracker.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthTracker : MonoBehaviour {
+	public int playerShips = 5;
+	public int enemyShips = 5;
+
+	public GameObject explosion;
+
+	public void loseGame () {
+
+	}
+
+	public void winGame () {
+		// Instantiate(explosion, new Vector3(transform.position.x + Random.Range()))
+	}
+
+	public void checkGameState() {
+		if (playerShips == 0)
+			loseGame();
+		else if (enemyShips == 0)
+			winGame();
+	}
+
+	public void removePlayerShip() {
+		--playerShips;
+	}
+
+	public void removeEnemyShip() {
+		--enemyShips;
+	}
+
+	void Update () {
+		checkGameState();
+	}
+}
