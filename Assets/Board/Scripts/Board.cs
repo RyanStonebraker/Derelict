074e7c705e3b78bb1585d100b26afdb6f80b29ae
orthogonaltrajectory@gmail.com <orthogonaltrajectory@gmail.com>
2018-02-23 14:03:34 -0900

    Adds movement to board when game start
    Needs testing

--- Assets/Board/Scripts/Board.cs
+++ Assets/Board/Scripts/Board.cs
@@ -15,7 +15,19 @@ using UnityEngine;
 public class Board : MonoBehaviour {
 
     public List<GameObject> nodes = new List<GameObject>(100);
-	
+
+    public IEnumerator MoveToPosition(Transform transform, Vector3 position, float timeToMove)
+    {
+        var currentPos = transform.position;
+        var t = 0f;
+        while (t < 1)
+        {
+            t += Time.deltaTime / timeToMove;
+            transform.position = Vector3.Lerp(currentPos, position, t);
+            yield return null;
+        }
+    }
+
     public int length()
     {
         return nodes.Count;
